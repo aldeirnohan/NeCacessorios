ced0b2b6334af9c92da006380c65dcdf3a41bd9b
aldeirnohan <aldeir_nohan03@live.com>
2020-06-06 17:00:42 -0300

    Update conexao.php

--- conexao/conexao.php
+++ conexao/conexao.php
@@ -1,8 +1,8 @@
 <?php
-$server = "localhost";
-$user="postgres";
-$password="postgres";
-$dbname="NeC";
+$server = "ec2-3-222-150-253.compute-1.amazonaws.com";
+$user="rqadityykhgrfy";
+$password="237c29531a67746652d8ac528701e737788d2cd038e479d97d3ac09b2c6348da";
+$dbname="d1gl4jgemb52hg";
 $port="5432";
 
 $conex= pg_connect("host=$server dbname=$dbname user=$user "."password=$password port=$port sslmode=disable");
